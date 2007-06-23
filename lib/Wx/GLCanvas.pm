928bd9b0b52941f67b3e2889aaad257f3a0c364e
Mattia Barbon <mbarbon@users.sourceforge.net>
2007-06-23 20:09:59 +0000

    Import of MBARBON/Wx-GLCanvas-0.06 from CPAN.
    gitpan-cpan-distribution: Wx-GLCanvas
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         MBARBON/Wx-GLCanvas-0.06.tar.gz
    gitpan-cpan-author:       MBARBON
    gitpan-cpan-maturity:     released

--- lib/Wx/GLCanvas.pm
+++ lib/Wx/GLCanvas.pm
@@ -1,11 +1,11 @@
 #############################################################################
-## Name:        GLCanvas.pm
+## Name:        lib/Wx/GLCanvas.pm
 ## Purpose:     loader for Wx::GLCanvas.pm
 ## Author:      Mattia Barbon
 ## Modified by:
 ## Created:     26/07/2003
-## RCS-ID:      $Id: GLCanvas.pm,v 1.2 2006/11/06 11:12:56 mbarbon Exp $
-## Copyright:   (c) 2003, 2005 Mattia Barbon
+## RCS-ID:      $Id: GLCanvas.pm 2061 2007-06-23 20:05:48Z mbarbon $
+## Copyright:   (c) 2003, 2005, 2007 Mattia Barbon
 ## Licence:     This program is free software; you can redistribute it and/or
 ##              modify it under the same terms as Perl itself
 #############################################################################
@@ -16,7 +16,7 @@ use strict;
 use Wx;
 use base 'Wx::ScrolledWindow';
 
-$Wx::GLCanvas::VERSION = '0.05';
+$Wx::GLCanvas::VERSION = '0.06';
 
 Wx::load_dll( 'gl' );
 Wx::wx_boot( 'Wx::GLCanvas', $Wx::GLCanvas::VERSION );
@@ -42,6 +42,11 @@ wxPerl distribution (wxGLCanvas).
 =head1 AUTHOR
 
 Mattia Barbon <mbarbon@cpan.org>
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
 
 =cut
 
